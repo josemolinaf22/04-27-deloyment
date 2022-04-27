8ef57bcef66d58a9369f3bb4c66ade3f2b9ee504
Jose Molina <josemolinaf22@gmail.com>
2022-04-27 15:19:05 -0600

    second upload

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const path = require('path')
 
 const app = express()
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, '../index.html'))
+app.get('/js', (req, res)=> {
+    res.sendFile(path.join(__dirname, './public/index.js'))
 })
 
 const port = process.env.PORT || 4005
