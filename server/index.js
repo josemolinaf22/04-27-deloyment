feb465d9b94819083409cb228a8bda055fdc7c59
Jose Molina <josemolinaf22@gmail.com>
2022-04-27 15:30:43 -0600

    third attempt

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const path = require('path')
 
 const app = express()
 
-app.get('/js', (req, res)=> {
-    res.sendFile(path.join(__dirname, './public/index.js'))
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
-
+app.get('/css',(req, res)=>{
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
